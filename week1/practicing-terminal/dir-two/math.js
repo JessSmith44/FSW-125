711b35f819f45d533fd4dd425619fe5b90dbc628
JessSmith44 <91652487+JessSmith44@users.noreply.github.com>
2022-03-28 20:16:49 -0600

    Add files via upload

---
+++ week1/practicing-terminal/dir-two/math.js
@@ -0,0 +1,46 @@
+<<<<<<< HEAD
+const sum = (a, b) => {
+    return a + b
+};
+
+const sub = (c, d) => {
+    return c - d
+};
+
+const mult = (e, f) => {
+    return e * f
+};
+
+const divide = (g, h) => {
+    return g / h
+};
+
+module.exports = {
+    sum,
+    sub,
+    mult,
+    divide
+=======
+const sum = (a, b) => {
+    return a + b
+};
+
+const sub = (c, d) => {
+    return c - d
+};
+
+const mult = (e, f) => {
+    return e * f
+};
+
+const divide = (g, h) => {
+    return g / h
+};
+
+module.exports = {
+    sum,
+    sub,
+    mult,
+    divide
+>>>>>>> 2554addc176e94bc4de8620e3f986b740e369714
+};
